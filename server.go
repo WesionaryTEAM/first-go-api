46578d80bfb132db4f0638f2aef52a987932e577
Anish <sunaranish17@gmail.com>
2020-08-05 16:39:38 +0545

    Logger Implemented

--- server.go
+++ server.go
@@ -4,7 +4,9 @@ import (
 	"go-jwt/controller"
 	"go-jwt/middlewares"
 	"go-jwt/service"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,13 +16,22 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
+
+	setupLogOutput()
 
 	var loginService service.LoginService = service.NewLoginService()
 	var jwtService service.JWTService = service.NewJWTService()
 	var loginController controller.LoginController = controller.LoginHandler(loginService, jwtService)
 
 	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
 
 	//Login Endpoint: Authentication + Token Creation
 	server.POST("/login", func(ctx *gin.Context) {
@@ -35,37 +46,37 @@ func main() {
 	})
 
 	//JWT Authorization Middleware applies to "/api" only
-	apiRoutes := server.Group("/api", middlewares.AuthorizeJWT())
-	{
-		apiRoutes.GET("/videos", func(ctx *gin.Context) {
-			ctx.JSON(200, videoController.FindAll())
-		})
+	// apiRoutes := server.Group("/api", middlewares.AuthorizeJWT())
+	// {
+	// 	apiRoutes.GET("/videos", func(ctx *gin.Context) {
+	// 		ctx.JSON(200, videoController.FindAll())
+	// 	})
 
-		apiRoutes.POST("/videos", func(ctx *gin.Context) {
-			err := videoController.Save(ctx)
-			if err != nil {
-				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			} else {
-				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid"})
-			}
-		})
-	}
+	// 	apiRoutes.POST("/videos", func(ctx *gin.Context) {
+	// 		err := videoController.Save(ctx)
+	// 		if err != nil {
+	// 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 		} else {
+	// 			ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid"})
+	// 		}
+	// 	})
+	// }
 
 	// server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
 
-	// server.GET("/videos", func(c *gin.Context) {
-	// 	c.JSON(200, videoController.FindAll())
-	// })
+	server.GET("/videos", func(c *gin.Context) {
+		c.JSON(200, videoController.FindAll())
+	})
 
-	// server.POST("/videos", func(c *gin.Context) {
-	// 	err := videoController.Save(c)
-	// 	if err != nil {
-	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	} else {
-	// 		c.JSON(http.StatusOK, gin.H{"message": "Video Input is valid"})
-	// 	}
+	server.POST("/videos", func(c *gin.Context) {
+		err := videoController.Save(c)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "Video Input is valid"})
+		}
 
-	// })
+	})
 
 	server.Run(":8080")
 }
