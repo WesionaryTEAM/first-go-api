5c8a9cd0c7410a302591093188b5c6e14ee340ff
Anish <sunaranish17@gmail.com>
2020-08-25 18:27:09 +0545

    Sending a mail from our golang application

---
+++ gomail/gmail_api.go
@@ -0,0 +1,44 @@
+package gomail
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
+)
+
+var GmailService *gmail.Service
+
+func OAuthGmailService() {
+	config := oauth2.Config{
+		ClienID:      os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		Endpoint:     google.Endpoint,
+		RedirectURL:  "http://localhost",
+	}
+
+	token := oauth2.Token{
+		AccessToken:  os.Getenv("ACCESS_TOKEN"),
+		RefreshToken: os.Getenv("REFRESH_TOKEN"),
+		TokenType:    "Bearer",
+		Expiry:       time.Now(),
+	}
+
+	var tokenSource = config.TokenSource(context.Background(), &token)
+
+	srv, err := gmail.NewService(context.Background(), option.WithTokenSource(tokenSource))
+	if err != nil {
+		log.Printf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	GmailService = srv
+	if GmailService != nil {
+		fmt.Println("Email Service is initiated")
+	}
+}
