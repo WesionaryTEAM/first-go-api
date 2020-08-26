fb7e359f50e81b587fb608a19fa2b7ff644a8331
Anish <sunaranish17@gmail.com>
2020-08-26 13:30:52 +0545

    Program executed successfully

--- gomail/gmail_api.go
+++ gomail/gmail_api.go
@@ -2,6 +2,8 @@ package gomail
 
 import (
 	"context"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -17,7 +19,7 @@ var GmailService *gmail.Service
 
 func OAuthGmailService() {
 	config := oauth2.Config{
-		ClienID:      os.Getenv("CLIENT_ID"),
+		ClientID:     os.Getenv("CLIENT_ID"),
 		ClientSecret: os.Getenv("CLIENT_SECRET"),
 		Endpoint:     google.Endpoint,
 		RedirectURL:  "http://localhost",
@@ -41,4 +43,28 @@ func OAuthGmailService() {
 	if GmailService != nil {
 		fmt.Println("Email Service is initiated")
 	}
+}
+
+func SendEmailOAUTH2(to string, data interface{}, template string) (bool, error) {
+	emailBody, err := parseTemplate(template, data)
+	if err != nil {
+		return false, errors.New("Unable to parse email template")
+	}
+
+	var message gmail.Message
+
+	emailTo := "To: " + to + "\r\n"
+	subject := "Subject: " + "Test Email from Gmail API using OAuth" + "\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
+	msg := []byte(emailTo + subject + mime + "\n" + emailBody)
+
+	message.Raw = base64.URLEncoding.EncodeToString(msg)
+
+	//Send the message
+	_, err = GmailService.Users.Messages.Send("me", &message).Do()
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
